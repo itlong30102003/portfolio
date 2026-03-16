9fa2b5bc40174adc1553845cbd7d131b7c9491d9
Tran Duc Long <tranduclong.se@gmail.com>
2026-03-16 23:54:55 +0700

    ++

--- algorithms/TwoSumII.java
+++ algorithms/TwoSumII.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0;
-        int right= numbers.length-1;
+        int right = numbers.length - 1;
 
-        while(left<right){
-            int currentsum = numbers[left]+numbers[right];
-            if(currentsum == target){
-                return new int[]{left+1,right+1};
-            }
-            else if(currentsum < target){
+        while (left < right) {
+            int currentsum = numbers[left] + numbers[right];
+            if (currentsum == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (currentsum < target) {
                 left++;
-            }
-            else {
+            } else {
                 right--;
             }
         }
